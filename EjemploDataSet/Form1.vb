6957001369035ac8b650b3870c63f2a378379de0
Ander Maestre <andermaestre_90@hotmail.es>
2019-11-15 13:02:01 +0100

    Ejemplo Dani

---
+++ EjemploDataSet/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListView1.Clear()
+        LlenarColumnas(ListView1)
+        LLenarFilas(ListView1)
+    End Sub
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        daUsuarios.Fill(ds, "Usuarios")
+        Dim t As DataTable = ds.Tables("Usuarios")
+        Dim key(0) As DataColumn
+        key(0) = t.Columns("IdUsuario")
+        t.PrimaryKey = key
+    End Sub
+
+    Private Sub MantenimientoUsuariosToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MantenimientoUsuariosToolStripMenuItem.Click
+        frmMantenimiento.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListView1.Clear()
+        LlenarColumnas(ListView1)
+        LLenarFilasOriginal(ListView1)
+    End Sub
+
+    Private Sub CancelarCambiosToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CancelarCambiosToolStripMenuItem.Click
+        ds.RejectChanges()
+    End Sub
+
+    Private Sub GuardarCambiosToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GuardarCambiosToolStripMenuItem.Click
+        Try
+            daUsuarios.Update(ds.Tables("Usuarios"))
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
---
+++ EjemploDataSet/Module1.vb
@@ -0,0 +1,60 @@
+﻿Module Module1
+    Public cadena As String = "Data Source=SEGUNDO150;Initial Catalog=DaniDB;Integrated Security=True"
+    Public daUsuarios As New SqlDataAdapter("SELECT * FROM Foro.Usuarios", cadena)
+    Public builder As New SqlCommandBuilder(daUsuarios)
+    Public ds As New DataSet
+    Public Sub InicializarDataAdapter()
+        daUsuarios.UpdateCommand = builder.GetUpdateCommand()
+        daUsuarios.DeleteCommand = builder.GetDeleteCommand()
+        daUsuarios.InsertCommand = builder.GetInsertCommand()
+    End Sub
+    Public Sub LLenarFilas(listView1 As ListView)
+        For Each r As DataRow In ds.Tables("Usuarios").Rows
+
+            Dim it As ListViewItem
+            If r.RowState = DataRowState.Deleted Then
+                it = listView1.Items.Add("----")
+            Else
+                it = listView1.Items.Add(r("idUsuario").ToString)
+            End If
+            For i As Integer = 1 To ds.Tables("Usuarios").Columns.Count - 1
+                If r.RowState = DataRowState.Deleted Then
+                    it.SubItems.Add("----")
+                Else
+                    it.SubItems.Add(r(i).ToString)
+                End If
+            Next
+
+            it.SubItems.Add(r.RowState.ToString)
+        Next
+
+    End Sub
+
+
+    Public Sub LlenarColumnas(listView1 As ListView)
+        For Each c As DataColumn In ds.Tables("Usuarios").Columns
+            listView1.Columns.Add(c.ColumnName)
+        Next
+        listView1.Columns.Add("RowState")
+    End Sub
+
+
+    Public Sub LLenarFilasOriginal(listView1 As ListView)
+        For Each r As DataRow In ds.Tables("Usuarios").Rows
+            If r.RowState = DataRowState.Deleted Then
+                Exit Sub
+            End If
+            Dim it As ListViewItem
+
+            it = listView1.Items.Add(r("idUsuario", DataRowVersion.Original).ToString)
+
+            For i As Integer = 1 To ds.Tables("Usuarios").Columns.Count - 1
+
+                it.SubItems.Add(r(i, DataRowVersion.Original).ToString)
+
+            Next
+
+            it.SubItems.Add(r.RowState.ToString)
+        Next
+    End Sub
+End Module
